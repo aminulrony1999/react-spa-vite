2bebe9fc613ed1708557d964460b4acb92ebd7c5
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-04 13:43:30 +0600

    useState and useEffect added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 const Shop = () => {
+    const [products, setProducts]  = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    },[]);
     return (
         <div className = 'shop-container'>
             <div className="products-container">
