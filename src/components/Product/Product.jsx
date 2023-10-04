585d26bba1aebe87125d006d5f3dacd86ad9c825
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-04 17:05:19 +0600

    products details are succeffully showing in the UI

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
-    const {name,price,stock,ratings} = props.product;
+    const {img,name,price,stock,ratings,seller, quantity} = props.product;
     return (
-        <div>
+        <div className = "product">
+            <img src={img} alt="Fetching image from api failed." />
             <h2>{name}</h2>
             <h3>Price : {price}</h3>
             <h3>Available stock : {stock}</h3>
