a3d1bd062b227a00aa7eb592f5552a85bb004412
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-04 19:17:49 +0600

    cart option added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     const {img,name,price,stock,ratings,seller, quantity} = props.product;
+
     return (
         <div className = "product">
             <img src={img} alt="Fetching image from api failed." />
@@ -11,6 +12,7 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating : {ratings} Stars</p>
             </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,20 +3,25 @@ import './Shop.css';
 import Product from '../Product/Product';
 const Shop = () => {
     const [products, setProducts]  = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data));
     },[]);
+    const handleAddToCart = (product) =>{
+    setCart([...cart,product]);
+    }
     return (
         <div className = 'shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product product = {product} key ={product.id}></Product>)
+                    products.map(product => <Product product = {product} key ={product.id} handleAddToCart = {handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
