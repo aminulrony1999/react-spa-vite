f7a4ac7540be4149e6196301b8c599efd9bdbf1b
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-04 11:30:28 +0600

    header and anchor tag added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header/Header'
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <Header></Header>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 const Header = () => {
     return (
         <div className = 'header'>
             <img src={logo} alt="" />
+            <a href="1">1</a>
+            <a href="2">2</a>
+            <a href="3">3</a>
+            <a href="4">4</a>
         </div>
     );
 };
