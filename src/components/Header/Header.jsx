3a7151a63468e173766baa3bc6f13006ba182c18
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-04 11:06:44 +0600

    header and logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './Header.css';
+import logo from '../../images/Logo.svg';
 const Header = () => {
     return (
-        <div>
-            <h3>This is a header from component file.</h3>
+        <div className = 'header'>
+            <img src={logo} alt="" />
         </div>
     );
 };
