cb35481c983846fcbca93cf43f1e4483109bc3c5
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-05 13:11:24 +0600

    total price of product in cart calculated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 const Cart = (props) => {
     const {cart} = props;
+    const total = cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price : </p>
+            <p>Total Shipping : </p>
+            <p>Tax</p>
+            <h6>Grand Total :{total}</h6>
         </div>
     );
 };
