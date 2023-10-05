3b96924879a28eabac3392b8bb936ea6eb1c28dc
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-05 14:39:57 +0600

    local storage integrated

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts]  = useState([]);
     const [cart, setCart] = useState([]);
@@ -10,9 +11,12 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data));
     },[]);
+
     const handleAddToCart = (product) =>{
     setCart([...cart,product]);
+    addToDb(product.id);
     }
+
     return (
         <div className = 'shop-container'>
             <div className="products-container">
