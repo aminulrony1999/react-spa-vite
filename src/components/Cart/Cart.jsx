e84245f4e94f499e291e31f1256bdae3971f9ae8
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-05 13:21:29 +0600

    cart will be stick  to the top

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 const Cart = (props) => {
     const {cart} = props;
     const total = cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
+    const totalShipping = cart.reduce((accumulator, currentValue) => accumulator+ currentValue.shipping, 0);
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
             <p>Total Price : </p>
-            <p>Total Shipping : </p>
-            <p>Tax</p>
+            <p>Total Shipping : {totalShipping}</p>
             <h6>Grand Total :{total}</h6>
         </div>
     );
