a22187148235bd2fc4056a2a31f45ef49d2f9c74
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-05 13:37:55 +0600

    some error fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 
 const Cart = (props) => {
     const {cart} = props;
-    const total = cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
+    const totalPrice = cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
     const totalShipping = cart.reduce((accumulator, currentValue) => accumulator+ currentValue.shipping, 0);
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price : </p>
-            <p>Total Shipping : {totalShipping}</p>
-            <h6>Grand Total :{total}</h6>
+            <p>Toatl Price : ${totalPrice}</p>
+            <p>Shipping : ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
