f37dcbf8aab7869d96135a3c3e1d387d62549588
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-06 14:37:11 +0600

    finished

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 const Cart = (props) => {
     const {cart} = props;
-    const totalPrice = cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
-    const totalShipping = cart.reduce((accumulator, currentValue) => accumulator+ currentValue.shipping, 0);
+
+    const totalPrice = cart.reduce((accumulator, currentValue) => accumulator + (currentValue.price * (currentValue.quantity || 1)), 0);
+    const totalShipping = cart.reduce((accumulator, currentValue) => accumulator+ (currentValue.shipping * (currentValue.quantity || 1)), 0);
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax;
+    const totalProduct = cart.reduce((accumulator, currentValue) => accumulator+ (currentValue.quantity || 1), 0);
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {totalProduct}</p>
             <p>Toatl Price : ${totalPrice}</p>
             <p>Shipping : ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
