ae2176b3180b980c0c442964513fd3ad252a4acf
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-12 15:28:27 +0600

    router is working successfully

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import {
 import Shop from './components/Shop/Shop.jsx';
 import Home from './components/Layout/Home.jsx';
 import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
 
 const router = createBrowserRouter([
     {
@@ -20,8 +22,16 @@ const router = createBrowserRouter([
           element : <Shop></Shop>
         },
         {
-          path : '/orders',
+          path : 'orders',
           element : <Orders></Orders>
+        },
+        {
+          path : 'inventory',
+          element : <Inventory></Inventory>
+        },
+        {
+          path : 'login',
+          element : <Login></Login>
         }
       ]
     }
