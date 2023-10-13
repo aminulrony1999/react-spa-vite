52bac41ef967c423b73fac65c743031fbd6058c8
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-13 13:51:24 +0600

    cart is sent successfully

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
+import cartProductsLoader from '../../loaders/cartProductsLoader';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h2>Orders page</h2>
+                <h2>Orderder amount {products.length}</h2>
             </div>
             <div className='cart-container'>
                 <Cart cart ={[]}></Cart>
